ceb40aa8e88d2c8775460ddb1649484c40557dc4
fulacse <1335857832@qq.com>
2023-06-16 11:09:01 +0200

    sinflifié les DonneesBloquees.java

--- donnéesBloquée/serveur/DonneesBloquees.java
+++ donnéesBloquée/serveur/DonneesBloquees.java
@@ -3,6 +3,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DonneesBloquees implements ServiceDonneesBloquees{
     @Override
     public HttpResponseDate fetch() throws Exception {
@@ -10,6 +14,18 @@ public class DonneesBloquees implements ServiceDonneesBloquees{
         HttpClient httpClient= HttpClient.newHttpClient();
         HttpRequest httpRequest= HttpRequest.newBuilder().uri(URI.create(url)).build();
         HttpResponse<String> response=httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        return new HttpResponseDate(response.statusCode(), response.body(), response.headers().firstValue("Content-Type").orElse("Unknown"));
+        String body=response.body();
+        JSONArray jsonArray= new JSONArray(body);
+        JSONArray filtre=new JSONArray();
+        for (int i=0; i<jsonArray.length(); i++){
+            JSONObject jsonObject=jsonArray.getJSONObject(i);
+            try {
+                String uucr_nom=jsonObject.getJSONObject("fields").getString("uucr_nom");
+                if (uucr_nom.equals("Nancy"))filtre.put(jsonObject);
+            }catch (JSONException e) {
+                //System.out.println("Pas de nom");
+            }
+        }
+        return new HttpResponseDate(response.statusCode(), filtre.toString(), response.headers().firstValue("Content-Type").orElse("Unknown"));
     }
 }
