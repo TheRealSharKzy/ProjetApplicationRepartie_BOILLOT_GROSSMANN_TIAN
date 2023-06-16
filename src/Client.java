bbf058faff1cd84343afdbd93b0e82ef1ec9f2c8
grossman3u <j06092003@gmail.com>
2023-06-16 11:48:23 +0200

    ajout reservation

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class Client {
@@ -12,6 +13,7 @@ public class Client {
          Registry reg = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
          ServiceBD bd = (ServiceBD) reg.lookup("BD");
          System.out.println(bd.getRestaurants());
+         System.out.println(bd.reserverTable(3,"Boillot","Thomas",3,"0666666666","2023-06-17"));
 
     }
 }
